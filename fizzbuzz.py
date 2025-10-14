76403bbb47da5a9d462f0b132acee397fac56918
KOALAS2648 <Koalak1d361@gmail.com>
2025-10-14 13:09:32 +0100

    working code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,14 @@
-{KNOW HOW to do it}
+def fizzbuzz(number):
+    if number % 15 == 0:
+        return "fizzBuzz"
+    elif number % 3==0:
+        return "fizz"
+    elif number %5==0:
+        return "buzz"
+    
+    else:
+        return number
+
+while True:
+    ask =  int(input("enter a number: "))
+    print(fizzbuzz(ask))
