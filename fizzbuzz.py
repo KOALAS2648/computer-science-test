665495de205e1af1c4ee9cbf8fd3290c4d86a091
KOALAS2648 <Koalak1d361@gmail.com>
2025-10-14 13:45:43 +0100

    added the proper code to get the marks

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,7 +8,10 @@ def fizzbuzz(number):
     
     else:
         return number
-
-while True:
-    ask =  int(input("enter a number: "))
-    print(fizzbuzz(ask))
+x = 0
+while x != 100:
+    ask =  input(f"enter if {x} is fizz, buzz or fizzbuzz: "))
+    if ask != fizzbuzz(x):
+        print("inncorrect")
+        print(f"you neededto answer {fizzbuzz(x)}")
+    x  += 1
